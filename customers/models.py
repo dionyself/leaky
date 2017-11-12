c771b35016e34f614a43e29c2574af4f1a267579
Dionys Rosario <dionyself@gmail.com>
2017-11-12 14:03:00 -0400

    removing initial migrations

--- customers/models.py
+++ customers/models.py
@@ -1,12 +1,32 @@
 from django.db import models
+from django.contrib.postgres.fields import JSONField
 from tenant_schemas.models import TenantMixin
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=100, help_text="Group Name.")
+    description = models.CharField(max_length=200, null=True, blank=True)
+    properties = JSONField(null=True, blank=True, help_text="The group properties.")
+    related_group = models.ForeignKey("self", null=True)
+    phone = models.CharField(max_length=100, help_text="Local Phone.")
+    is_active = models.BooleanField(default=True)
 
 
 class Client(TenantMixin):
     created_on = models.DateField(auto_now_add=True)
-    name = models.CharField(max_length=100)
-    phone = models.CharField(max_length=100)
-    location = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100, help_text="Local Name.")
+    group = models.ForeignKey(Group, null=True, blank=True, help_text="Corporation")
+    phone = models.CharField(max_length=100, help_text="Local Phone.")
+    latitude = models.FloatField(null=True, blank=True, help_text="coordinates.")
+    longitude = models.FloatField(null=True, blank=True, help_text="coodinates.")
     time_zone = models.CharField(max_length=100, null=True, blank=True)
     is_active = models.BooleanField(default=True)
     auto_create_schema = True
+
+
+class Product(models.Model):
+    code = models.IntegerField(default=5, help_text="Universal barcode.")
+    code_type = models.TextField(help_text="Barcode type.")
+    existence_type = models.TextField(help_text="Existence type (Pound, MB, centimeter).")
+    name = models.TextField(help_text="Product name.")
+    properties = JSONField(null=True, blank=True, help_text="The event properties.")
--- warehouses/models.py
+++ warehouses/models.py
@@ -1,19 +1,21 @@
-#from djmoney.models.fields import MoneyField
 from django.db import models
-
-# Create your models here.
+from djmoney.models.fields import MoneyField
+from django.contrib.postgres.fields import JSONField
 
 
 class Asset(models.Model):
-    price = models.IntegerField(default=5, help_text="The Asset price.")
-    #price = MoneyField(max_digits=10, decimal_places=2, default_currency='USD')
-    existence = models.IntegerField(default=5, help_text="The Asset price.")
-    # properties = JSONField(null=True, blank=True, help_text="The event properties.")
-    code = models.TextField(help_text="custom_code.")
-    product_id = models.IntegerField(default=5, help_text="The Asset price.")
-    type = models.TextField(help_text="The assetType name.")
-    expires_at = models.DateTimeField()
-    duration = models.DurationField()
+    price = MoneyField(max_digits=10, decimal_places=2, default_currency='USD', help_text="The Asset price.")
+    existence = models.IntegerField(default=0, help_text="Amount of assets available.")
+    properties = JSONField(null=True, blank=True, help_text="Using custom properties for local assets only")
+    code = models.TextField(default="", help_text="custom_code.")
+    product_id = models.IntegerField(null=True, help_text="Global product_id.")
+    asset_type = models.TextField(default="product", help_text="Asset type: product/service/subscription/warranty")
+    category = models.TextField(null=True, blank=True, help_text="Department/location of the product")
+    duration = models.DurationField(null=True, blank=True, help_text="Duration of a given offer")
+    expires_at = models.DateTimeField(null=True, blank=True,
+        help_text="Expiration date of an offer/product: (exprires_at = created_at + duration)")
+    is_deleted = models.BooleanField(default=False, help_text="Assets cannot be deleted, just mark it as deleted")
+    is_expired = models.BooleanField(default=False, help_text="Force the Asset to expire")
 
     class Meta:
         ordering = ["id"]
